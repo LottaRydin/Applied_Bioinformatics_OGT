6b5a75b103c0968dc8b30890693514ce2fa4d259
buinovsg <buinovsg@tcd.ie>
2021-11-19 12:56:08 +0100

    cleaned up the script a little

--- 1000_samples/data_plots.py
+++ 1000_samples/data_plots.py
@@ -6,49 +6,47 @@ import seaborn as sns
 import random
 from collections import Counter
 
-#load data
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
-
+##load data
+os.chdir(os.path.dirname(os.path.realpath(__file__))) #set current dir to script location
 tempura_otu_df = pd.read_csv("matched_temp_multi_v2.tsv", sep='\t')
 tempura_otu_df = tempura_otu_df.set_index(["OTU_id"])
-
+tempura = pd.read_csv("TEMPURA.csv") #load entire tempura db
 metadata = pd.read_csv('temp_samples.tsv', sep='\t') #specify metadata file
 metadata = metadata.drop(columns=['Unnamed: 0']) #drop unnecessary col
-
 pipeline_map = pd.read_csv('both_pipes.tsv', sep='\t')
-#pipeline_map = pipeline_map.set_index(["OTU_id_5"]) #set OTU col as the index
-
-tempura = pd.read_csv("TEMPURA.csv") #load all tempura data
-
 df = pd.read_csv("data_whole.tsv", sep='\t') #compile_data_medium.py output
+
+## converting v5 otu id to v4.1
 #which v5 otu not in pipeline_map, remove this data
 list_1 = pipeline_map.OTU_id_5.tolist() #all v5 otu id in map file
 list_2 = df.loc[df.pipeline == 5, :].OTU_ID.unique() #list of v5 otu from tsv files
 missing_v5_id = list(set(list_2).difference(list_1))
 df = df.drop(df[(df['pipeline'] == 5) & (df['OTU_ID'].isin(missing_v5_id))].index) #removed 400 rows because no mtching v5 otu id in the map
-#not convert v5 otu id to v4.1
-otu_dict = dict(zip(pipeline_map.OTU_id_5, pipeline_map.OTU_id_4_1))
-df.loc[df.pipeline == 5, 'OTU_ID'] = df['OTU_ID'].map(otu_dict)
-#df_5 = df.loc[df.pipeline == 5, :]
+#now convert v5 otu id to v4.1
+otu_dict = dict(zip(pipeline_map.OTU_id_5, pipeline_map.OTU_id_4_1)) #dict which links v5 id t ov4.1 id
+df.loc[df.pipeline == 5, 'OTU_ID'] = df['OTU_ID'].map(otu_dict) #in rows where pipeline == 5, convert otu id based on dict
+df["OTU_ID"] = df["OTU_ID"].astype(int, errors = 'raise')
 df = df.set_index(["OTU_ID"]) #set OTU col as the index
 df.sort_index(inplace=True) #sort dataframe based on increasing index values
+del list_1
+del list_2
+del otu_dict
 
-df.index.unique("OTU_ID") #unique OTU ID indices from the appended_df
-df.groupby(level="OTU_ID").size() #count data samples per OTU, pandas series output
-Counter(df.groupby(level="OTU_ID").size().tolist()) #how many OTUs have 1 data pt, 2 etc
-
+##fitering
 #filter out eukaryotes
 df = df[~df.taxonomy.str.contains("Eukaryota")]
-
 #Filter for number of samples per OTU
 #df.drop(df[df["abundance"] < 3].index, inplace = True) #drop sample with less that 3 reads
-
 #filter out OTUs with less than 10 observations
 OTU_size = df.groupby(level="OTU_ID").size()
 df = df.drop(OTU_size.index[OTU_size < 10].tolist())
 
+#data summary
+df.index.unique("OTU_ID") #unique OTU ID indices df
+df.groupby(level="OTU_ID").size() #count data samples per OTU, pandas series output
+Counter(df.groupby(level="OTU_ID").size().tolist()) #how many OTUs have 1 data pt, 2 etc
 ##############################################################################
-#plt violin
+#plt violin for specific otu s
 # for otu in random.sample(df.index.unique("OTU_ID").tolist(), 3):
 #         
 # otu_4572 = df.loc[4572, :].temp #species
@@ -67,9 +65,9 @@ df = df.drop(OTU_size.index[OTU_size < 10].tolist())
 # plt.show()
 # 
 ##############################################################################
-#seaborn plot
+#seaborn violin plot
 plot_df = []
-rand_otu = random.sample(df.index.unique("OTU_ID").tolist(), 10)
+rand_otu = random.sample(df.index.unique("OTU_ID").tolist(), 10) #pick 10 random otu ids
 for indx in rand_otu:
     df_otu = df.loc[indx, :]
     plot_df.append(df_otu)
@@ -81,8 +79,7 @@ ax = sns.violinplot(x="taxonomy", y="temp", data=plot_df, scale="count", inner="
 ax.tick_params(axis='x', rotation=90)
 
 ##############################################################################
-len(list(set(df.index.unique("OTU_ID")) & set(tempura_otu_df.index.unique("OTU_id"))))
-
+#plot scatter plot to compare otu data to tempura
 def tempura_and_prediction(OTU_nr):
     plt.figure()
     plt.scatter(df.loc[OTU_nr, :].temp, df.loc[OTU_nr, :].rel_abundance, alpha=0.5)
@@ -101,14 +98,12 @@ print("Out of", len(df.index.unique("OTU_ID")),"OTUs present in MGnify data", \
       len(list(set(df.index.unique("OTU_ID")) & set(tempura_otu_df.index.unique("OTU_id")))), "were present in TEMPURA" )
 
 rand_otu = random.sample(list(set(df.index.unique("OTU_ID")) & \
-                              set(tempura_otu_df.index.unique("OTU_id"))), 5)
+                              set(tempura_otu_df.index.unique("OTU_id"))), 5) #pick random ots which have data also in tempura
 for otu in rand_otu:
     tempura_and_prediction(otu)
  
 ################################################################################ 
-#plot how ratio of how close Topt is to Tmax in TEMPURA (but not all TEMPURA data, we use the file for OTU ID matching)
-# tempura_tmax = tempura_otu_df[tempura_otu_df.taxonomy.str.contains("sk__Bacteria")]["Tmax"]
-# tempura_tmin = tempura_otu_df[tempura_otu_df.taxonomy.str.contains("sk__Bacteria")]["Tmin"]
+#plot ratio of how close Topt is to Tmax in TEMPURA (but not all TEMPURA data, we use the file with 4.1 otu matches)
 # tempura_topt = tempura_otu_df[tempura_otu_df.taxonomy.str.contains("sk__Bacteria")]["Topt_ave"] #get almost same as below if ignore archea
 tempura_tmax = tempura_otu_df["Tmax"] #when a table with all TEMPURA data is used, get about same mean ratio
 tempura_tmin = tempura_otu_df["Tmin"]
@@ -126,8 +121,9 @@ plt.show()
 #tempura_ratio[tempura_ratio < 0] #how many ratios below 0, so that Topt is below Tmax
 #tempura_ratio[(tempura_ratio > 0) & (tempura_ratio < 0.1)] #Topt very close to Tmin
 ###############################################################################
-OTU_size.plot(kind='hist', bins=20, rwidth=0.9, color='#014d4e', \
-              title = "Number of observations of OTU" )
+#some summary histograms
+df.groupby(level="OTU_ID").size().plot(kind='hist', bins=30, rwidth=0.9, color='#014d4e', \
+              title = "Number of observations of OTU" ) #df.groupby(level="OTU_ID")
 plt.xlabel("observation nr")
 plt.show()
 
@@ -142,37 +138,32 @@ plt.xlabel("temperature")
 plt.show()
 
 ###############################################################################
-
+#combining MGnify and TEMPURA temp data
 cols = ['OTU_ID', 'Tmin', 'Topt', 'Tmax']
-temps_df = []
+temps_pred = []
 for otu in df.index.unique("OTU_ID"):
     optimum_est = min(df.loc[otu, :].temp) + (max(df.loc[otu, :].temp) - min(df.loc[otu, :].temp)) *0.66
     temps_est = [otu, min(df.loc[otu, :].temp), optimum_est, max(df.loc[otu, :].temp)]
-    temps_df.append(temps_est)
+    temps_pred.append(temps_est)
 
-temps_df = pd.DataFrame(temps_df, columns=cols)
-temps_df = temps_df.set_index(["OTU_ID"]) #set OTU col as the index
-temps_df.sort_index(inplace=True) #sort dataframe based on increasing index values
-temps_df["temp_Tmin"] = ""
-temps_df["temp_Topt"] = ""
-temps_df["temp_Tmax"] = ""
+temps_pred = pd.DataFrame(temps_pred, columns=cols)
+del cols
+del temps_est
+temps_pred = temps_pred.set_index(["OTU_ID"]) #set OTU col as the index
+temps_pred.sort_index(inplace=True) #sort dataframe based on increasing index values
 
 tempura_matches = list(set(df.index.unique("OTU_ID")) & set(tempura_otu_df.index.unique("OTU_id")))
+temps_compare = temps_pred.loc[tempura_matches]
+temps_compare = temps_compare.reindex(columns=['Tmin', 'Topt', 'Tmax', 'temp_Tmin', 'temp_Topt', 'temp_Tmax'])
+temps_compare.loc[:,'temp_Tmin':'temp_Tmax'] = tempura_otu_df.loc[temps_compare.index,'Tmin':'Tmax'].values
 
-for otu in tempura_matches:
-    temps_df.loc[otu,'temp_Tmin':'temp_Tmax'] = tempura_otu_df.loc[otu,'Tmin':'Tmax'].values
-    
-regression = temps_df.loc[tempura_matches]
-regression["temp_Tmin"] = regression["temp_Tmin"].astype(float, errors = 'raise')
-regression["temp_Topt"] = regression["temp_Topt"].astype(float, errors = 'raise')
-regression["temp_Tmax"] = regression["temp_Tmax"].astype(float, errors = 'raise')
 
 #from sklearn.model_selection import train_test_split 
 #from sklearn.linear_model import LinearRegression
 #from sklearn import metrics
 from sklearn.linear_model import LinearRegression
-X = regression.Topt.values.reshape(-1, 1)
-Y = regression.temp_Topt.values.reshape(-1, 1)
+X = temps_compare.Topt.values.reshape(-1, 1)
+Y = temps_compare.temp_Topt.values.reshape(-1, 1)
 linear_regressor = LinearRegression()  # create object for the class
 linear_regressor.fit(X, Y)  # perform linear regression
 Y_pred = linear_regressor.predict(X)  # make predictions
@@ -180,8 +171,8 @@ plt.scatter(X, Y)
 plt.plot(X, Y_pred, color='red')
 plt.show()
 
-X = regression.Tmin.values.reshape(-1, 1)
-Y = regression.temp_Tmin.values.reshape(-1, 1)
+X = temps_compare.Tmin.values.reshape(-1, 1)
+Y = temps_compare.temp_Tmin.values.reshape(-1, 1)
 linear_regressor = LinearRegression()  # create object for the class
 linear_regressor.fit(X, Y)  # perform linear regression
 Y_pred = linear_regressor.predict(X)  # make predictions
@@ -189,8 +180,8 @@ plt.scatter(X, Y)
 plt.plot(X, Y_pred, color='red')
 plt.show()
 
-X = regression.Tmax.values.reshape(-1, 1)
-Y = regression.temp_Tmax.values.reshape(-1, 1)
+X = temps_compare.Tmax.values.reshape(-1, 1)
+Y = temps_compare.temp_Tmax.values.reshape(-1, 1)
 linear_regressor = LinearRegression()  # create object for the class
 linear_regressor.fit(X, Y)  # perform linear regression
 Y_pred = linear_regressor.predict(X)  # make predictions
@@ -198,3 +189,6 @@ plt.scatter(X, Y)
 plt.plot(X, Y_pred, color='red')
 plt.show()
 
+
+
+    

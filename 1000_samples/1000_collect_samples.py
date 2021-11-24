607b7005f814348aa3566eb9464c2341350425f5
Lotta Rydin <lotta.rydin@telia.com>
2021-11-24 13:52:37 +0100

    catches most connection errors

--- 1000_samples/1000_collect_samples.py
+++ 1000_samples/1000_collect_samples.py
@@ -2,6 +2,7 @@ import argparse
 import csv
 import os
 import pandas
+import time
 
 try:
     from urllib import urlencode
@@ -23,8 +24,10 @@ if not os.path.exists("tsv_files"):
     os.mkdir("tsv_files")
 
 
-# Making dataframe for storing temperature data:
+# Making dataframes for storing temperature data and data from connection errors:
 temp_df = pandas.DataFrame(columns=['sample_accession', 'temperature', 'enviroment', 'sequencing_platform', 'pipeline_version', 'analysis_accession'])
+samples_error = pandas.DataFrame(columns=['sample_accession'])
+analysis_error = pandas.DataFrame(columns=['analysis_accession'])
 
 # Extracting infor from MGnify:
 with Session(API_BASE) as session:
@@ -36,9 +39,14 @@ with Session(API_BASE) as session:
 
     f = Filter(urlencode(params)) # Create filter
 
+    # sample_list=session.iterate('biomes/root:Environmental:Aquatic:Marine/samples', f)
+    # print('STARTING')
+    # time.sleep(5)
+    
     # Iterating through samples from salt marshes (enviroment) with temperature data availible
     for sample in session.iterate('biomes/root:Environmental:Aquatic:Marine/samples', f): #biomes/root:Environmental:Aquatic:Marine:Intermediate Zone/samples
-        if count == 1200 or add_count == 1000:
+    #for sample in sample_list:
+        if count == 12 or add_count == 1000:
             print(f'FINAL: Explored: {count}, downloaded: {add_count}')
             break
         elif add_count%50 == 0:
@@ -60,42 +68,55 @@ with Session(API_BASE) as session:
         backup_instrument_platform = None
         backup_pipeline_v = None
         backup_exists = False
+        
+        max_attempts = 3
+        for attempt in range(1, max_attempts+1):
+            try:   
+                for run in sample.runs:
+                    #print("run acc: ", run.accession, "pipeline: ", [p.release_version for p in run.pipelines], "experiment type: ", run.experiment_type)
+                    try:
+                        pipelines_used = [p.release_version for p in run.pipelines]
+                    except requests.exceptions.SSLError:
+                        print('Error rad 68!')
+        
+                    if ('4.1' in pipelines_used or '5.0' in pipelines_used) and run.experiment_type == 'amplicon':
 
-        for run in sample.runs:
-            #print("run acc: ", run.accession, "pipeline: ", [p.release_version for p in run.pipelines], "experiment type: ", run.experiment_type)
-            
-            pipelines_used = [p.release_version for p in run.pipelines]
- 
-            if ('4.1' in pipelines_used or '5.0' in pipelines_used) and run.experiment_type == 'amplicon':
+                        # Backup if keys are wrong. Done once per sample and takes first analysis.
+                        if run.analyses[0].pipeline_version in ['4.1', '5.0'] and not backup_exists:
+                            backup_analysis = run.analyses[0].accession
+                            backup_instrument_platform = run.analyses[0].instrument_platform
+                            backup_pipeline_v = run.analyses[0].pipeline_version
+                            backup_exists = True
 
-                # Backup if keys are wrong. Done once per sample and takes first analysis.
-                if run.analyses[0].pipeline_version in ['4.1', '5.0'] and not backup_exists:
-                    backup_analysis = run.analyses[0].accession
-                    backup_instrument_platform = run.analyses[0].instrument_platform
-                    backup_pipeline_v = run.analyses[0].pipeline_version
-                    backup_exists = True
-
-                for analysis in run.analyses:
-                    #print(f'ananlysis acc: {analysis.accession}, analysis pipeline: {analysis.pipeline_version}') #, analysis sum: {analysis.analysis_summary}')
-                    
-                    for e in analysis.analysis_summary:
-                        if e['key'] == 'Reads with predicted RNA' or e['key'] == 'Predicted SSU sequences': 
-                            if analysis.pipeline_version == '4.1':
-                                if int(e['value']) >= best_run_val:
-                                    best_run = run.accession
-                                    best_run_val = int(e['value'])
-                                    best_analysis = analysis.accession
-                                    pipeline_v = '4.1'
-                                    instrument_platform = analysis.instrument_platform
-                            elif analysis.pipeline_version == '5.0':
-                                if int(e['value']) >= best_run_val and (pipeline_v == None or pipeline_v == '5.0'):
-                                    best_run = run.accession
-                                    best_run_val = int(e['value'])
-                                    best_analysis = analysis.accession
-                                    pipeline_v = '5.0'
-                                    instrument_platform = analysis.instrument_platform
-            #else:
-                #print("------------------NOT A GOOD PIPELINE/EXP TYPE---------------------")
+                        for analysis in run.analyses:
+                            #print(f'ananlysis acc: {analysis.accession}, analysis pipeline: {analysis.pipeline_version}') #, analysis sum: {analysis.analysis_summary}')
+                            
+                            for e in analysis.analysis_summary:
+                                if e['key'] == 'Reads with predicted RNA' or e['key'] == 'Predicted SSU sequences': 
+                                    if analysis.pipeline_version == '4.1':
+                                        if int(e['value']) >= best_run_val:
+                                            best_run = run.accession
+                                            best_run_val = int(e['value'])
+                                            best_analysis = analysis.accession
+                                            pipeline_v = '4.1'
+                                            instrument_platform = analysis.instrument_platform
+                                    elif analysis.pipeline_version == '5.0':
+                                        if int(e['value']) >= best_run_val and (pipeline_v == None or pipeline_v == '5.0'):
+                                            best_run = run.accession
+                                            best_run_val = int(e['value'])
+                                            best_analysis = analysis.accession
+                                            pipeline_v = '5.0'
+                                            instrument_platform = analysis.instrument_platform
+                    #else:
+                        #print("------------------NOT A GOOD PIPELINE/EXP TYPE---------------------")
+            except (requests.exceptions.SSLError, requests.exceptions.ConnectionError):
+                print(f'Connection error when retrieving information about sample. Attempt: {attempt}, Sample: {sample.accession}')
+                if attempt == max_attempts:
+                    print(f'Maximum attempts for retrieving information about sample reached. {sample.accession} added to excess file')
+                    #add file
+                    samples_error = samples_error.append({'sample_accession': sample.accession}, ignore_index=True)
+            else:
+                break
             
         #print(f"best run: {best_run}, {best_run_val} in analysis: {best_analysis}")
 
@@ -110,41 +131,58 @@ with Session(API_BASE) as session:
             else:
                 ANALYSIS_ACCESSION = backup_analysis
                 print('******BACKUP USED*******')
-
-            analysis = session.get(RESOURCE, ANALYSIS_ACCESSION).resource
-
-            # this will iterate over the download files from the API for the analysis
-            # and will download only the otu tsv file.
-            #print(API_BASE + "/" + RESOURCE + "/" + ANALYSIS_ACCESSION + "/downloads")
-            for download in session.iterate(RESOURCE + "/" + ANALYSIS_ACCESSION + "/downloads"):
-                # print(f'download alias: {download.alias}')
-
-                if "FASTQ_SSU_OTU.tsv" in download.alias: # Only download tsv file
-                    output_file = os.path.join("tsv_files", sample.accession + ".tsv")
-                    
-                    # if os.path.exists(output_file):
-                    #     #print("Already downloaded: " + download.alias)
-                    #     continue
-                    #print("Downloading: " + download.alias)
-
-                    with requests.get(download.links.self) as response:
-                        response.raise_for_status()
-                        with open(output_file, "wb") as f:
-                            f.write(response.content)
-                    break # Break loop, file is found
             
-            # Add sample metadata to data frame after tsv file is downloaded
+            
+            for attempt in range(1, max_attempts+1):
+                try:
+                    #print("DOWNLOAD")
+                    #time.sleep(5)
+                    analysis = session.get(RESOURCE, ANALYSIS_ACCESSION).resource
 
-            env = sample.biome.id #sample.nvironment_material
-            tmp = 'foo'
-            for meta in sample.sample_metadata:
-                if meta['key'] == 'temperature':
-                    tmp = meta['value']
-                # elif meta['key'] == 'environment (material)':
-                #     env = meta['value'] 
-            temp_df = temp_df.append({'sample_accession': sample.accession, 'temperature': tmp, 'enviroment': env, 'sequencing_platform': instrument_platform, 'pipeline_version': pipeline_v, 'analysis_accession': ANALYSIS_ACCESSION }, ignore_index=True)
-            add_count += 1
+                    # this will iterate over the download files from the API for the analysis
+                    # and will download only the otu tsv file.
+                    #print(API_BASE + "/" + RESOURCE + "/" + ANALYSIS_ACCESSION + "/downloads")
+                    for download in session.iterate(RESOURCE + "/" + ANALYSIS_ACCESSION + "/downloads"):
+                        # print(f'download alias: {download.alias}')
+
+                        if "FASTQ_SSU_OTU.tsv" in download.alias: # Only download tsv file
+                            output_file = os.path.join("tsv_files", sample.accession + ".tsv")
+                            
+                            # if os.path.exists(output_file):
+                            #     #print("Already downloaded: " + download.alias)
+                            #     continue
+                            #print("Downloading: " + download.alias)
+
+                            with requests.get(download.links.self) as response:
+                                response.raise_for_status()
+                                with open(output_file, "wb") as f:
+                                    f.write(response.content)
+                            break # Break loop, file is found
+                    
+                    # Add sample metadata to data frame after tsv file is downloaded
+
+                    env = sample.biome.id #sample.nvironment_material
+                    tmp = 'foo'
+                    for meta in sample.sample_metadata:
+                        if meta['key'] == 'temperature':
+                            tmp = meta['value']
+                        # elif meta['key'] == 'environment (material)':
+                        #     env = meta['value'] 
+                    temp_df = temp_df.append({'sample_accession': sample.accession, 'temperature': tmp, 'enviroment': env, 'sequencing_platform': instrument_platform, 'pipeline_version': pipeline_v, 'analysis_accession': ANALYSIS_ACCESSION }, ignore_index=True)
+                    add_count += 1
+                    #print("DOWNLOAD FINISHED")
+                except (requests.exceptions.SSLError, requests.exceptions.ConnectionError):
+                    print(f'Connection error when dowloading TSV file for sample. Attempt: {attempt}, Analysis: {ANALYSIS_ACCESSION}')
+                    if attempt == max_attempts:
+                        print(f'Maximum attempts for dowloading TSV file for sample reached. {ANALYSIS_ACCESSION} added to excess file')
+                        analysis_error = analysis_error.append({'analysis_accession': ANALYSIS_ACCESSION }, ignore_index=True)
+                        best_analysis = None
+                        backup_exists = False
+                else:
+                    break
 temp_df.to_csv('temp_samples.tsv', sep='\t')
+samples_error.to_csv('samples_not_included', sep='\t')
+analysis_error.to_csv('analyses_not_downloaded', sep='\t')
 
             
 

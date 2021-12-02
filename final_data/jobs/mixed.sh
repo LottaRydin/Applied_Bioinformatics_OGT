1ce5854cc0d3e7d93ce810b2b9f8abf0f7ece9e6
Lotta Rydin <lotta.rydin@telia.com>
2021-12-02 11:13:00 +0100

    mixed download

---
+++ final_data/jobs/mixed.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#SBATCH -A snic2021-22-850
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 60:00:00
+#SBATCH -J mixed_job
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user lotta.rydin@telia.com
+
+# Load modules
+
+
+# Your commands
+python3 ../../Applied_Bioinformatics_OGT/final_data/collect_data_final.py
